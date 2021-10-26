5e16c03f2c460c5ea8ac1c601f3f4da3528077dd
Tether <tarun.sankhla.prc@gmail.com>
2021-10-26 23:48:51 +0530

    update script js  to call node server

--- js/script.js
+++ js/script.js
@@ -35,6 +35,33 @@ const sendVerificationCode = () => {
 
 const signInWithPhone = sentCodeId => {
   const code = codeField.value;
+
+  var obj ={sentCodeId,code}
+  console.log("object :: ",obj)
+  console.log("User Register");
+  const data = {otp: code,verificationId: sentCodeId};
+  const e = JSON.stringify(data);
+  console.log("e :: ",e)
+  //api
+  fetch(' http://localhost:8080/firebase', {
+    method: 'POST',
+    body:e,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    }
+  }).then(function (response) {
+    console.log("response ok !")
+    if (response.ok) {
+      return response.json();
+    }
+    return Promise.reject(response);
+  }).then(function (data) {
+    console.log("data then:",data);
+  }).catch(function (error) {
+    console.warn('Something went wrong.', error);
+  });
+  
+
   // A credential object (contains user's data) is created after a comparison between the 6 digit code sent to the user's phone
   // and the code typed by the user in the code field on the html form.
   const credential = firebase.auth.PhoneAuthProvider.credential(sentCodeId, code);
